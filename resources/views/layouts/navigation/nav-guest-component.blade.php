065c3f0f14e73e3b730ee7476e00baf8ec12e8f1
gezrylclariz <gezrylclarizg@gmail.com>
2021-09-29 13:23:40 +0800

    update gui login button in nav guest component

--- resources/views/layouts/navigation/nav-guest-component.blade.php
+++ resources/views/layouts/navigation/nav-guest-component.blade.php
@@ -22,7 +22,7 @@
             <div class="sm:flex sm:items-center sm:ml-6">
                 @if (Route::has('login'))
                     <div class="ml-3 relative lg:mt-0 sm:mt-0 mt-2">
-                        <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 border text-sm leading-4 font-medium rounded-sm text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                        <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 border text-sm leading-4 font-medium rounded-sm text-green-800 bg-green-200 hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
                             Log in
                         </a>
                         @if (Route::has('register'))
