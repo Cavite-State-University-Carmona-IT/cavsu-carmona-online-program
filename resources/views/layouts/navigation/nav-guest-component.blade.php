cdc48c2b00636c0ab254dad30f30d2bb155acc27
gezrylclariz <gezrylclarizg@gmail.com>
2021-09-29 13:25:48 +0800

    update login button in nav guest component

--- resources/views/layouts/navigation/nav-guest-component.blade.php
+++ resources/views/layouts/navigation/nav-guest-component.blade.php
@@ -23,7 +23,7 @@
                 @if (Route::has('login'))
                     <div class="ml-3 relative lg:mt-0 sm:mt-0 mt-2">
                         <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 border text-sm leading-4 font-medium rounded-sm text-green-800 bg-green-200 hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                            Log in
+                            Log in test
                         </a>
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="inline-flex items-center px-3 py-2 m-1 border text-sm leading-4 font-medium rounded-sm text-white bg-gray-700 hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
