2aea43751ff087ef7def2e1b74fd0a6835ca77fd
Carl Joshua Vicente Lareza <91177282+joshualareza@users.noreply.github.com>
2021-10-01 17:16:19 +0800

    wip-update-user-table
    updated the columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('address')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('employment_status')->nullable();
+            $table->string('username')->unique;
+            $table->string('email')->unique;
             $table->timestamp('email_verified_at');
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
+            $table->string('highest_eduactional_attainment')->nullable();
+            $table->string('income')->nullable();
+            $table->string('status')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
             $table->softDeletes();
