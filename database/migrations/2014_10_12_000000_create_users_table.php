333712b63ddc2d228bb981ccd731029918b3daac
Carl Joshua Vicente Lareza <91177282+joshualareza@users.noreply.github.com>
2021-10-04 18:07:07 +0800

    Revert "wip-update-user-table-columns"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,28 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name')->nullable();
-            $table->string('first_name')->nullable();
-            $table->string('middle_name')->nullable();
-            $table->string('gender')->nullable()->comment('ex. 0-male, 1-female');
-            $table->date('birth_date')->nullable();
-            $table->text('address')->nullable();
-            $table->string('marital_status')->nullable()->comment('ex. 1-single, 2-married, etc.');
-            $table->tinyInteger('employment_status')->nullable()->comment('ex. 0-employed, 1-student, etc.');
-            $table->string('username')->unique;
-            $table->string('email')->unique;
-            $table->timestamp('email_verified_at');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
-            $table->string('highest_eduactional_attainment')->nullable()->comment('ex. 1 - high school, 2- bachelor, etc.');
-            $table->integer('income')->nullable();
-            $table->tinyInteger('status')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
-            $table->softDeletes();
-
-            
         });
     }
 
