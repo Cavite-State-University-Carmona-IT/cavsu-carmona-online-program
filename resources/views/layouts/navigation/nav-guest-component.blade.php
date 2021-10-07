533ee1058aa7671c41b599d3cab41d8cbcece997
gezrylclariz <gezrylclarizg@gmail.com>
2021-10-07 10:36:08 +0800

    navigation login button change to sign in

--- resources/views/layouts/navigation/nav-guest-component.blade.php
+++ resources/views/layouts/navigation/nav-guest-component.blade.php
@@ -30,7 +30,7 @@
                                     @if (Route::has('login'))
                                         <div class="w-full flex">
                                             <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-sm text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                                                Log in
+                                                Sign in
                                             </a>
                                             @if (Route::has('register'))
                                                 <a href="{{ route('register') }}" class="inline-flex items-center px-3 py-2 m-1 text-sm leading-4 font-medium rounded-sm text-white bg-gray-700 hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
