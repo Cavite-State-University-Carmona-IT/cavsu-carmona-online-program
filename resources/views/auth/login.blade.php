6ca71c107ad27ac8e8cf9167c7eb2d43b3c56ea6
robis032794 <adanrobis@gmail.com>
2021-10-14 17:51:57 +0800

    x-jet-label, x-jet-input change to label and input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,13 +16,13 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <label for="email" value="{{ __('Email') }}" />
+                <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <label for="password" value="{{ __('Password') }}" />
+                <input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
