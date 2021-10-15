19a1c4dc923b7e8ab3cfc97b7d7d2ccc84c82094
kimmydelpozo <91210246+kimmydelpozo@users.noreply.github.com>
2021-10-15 14:33:41 +0800

    Remove Settings option

--- resources/views/layouts/navigation/nav-sidebar-program-coordinator.blade.php
+++ resources/views/layouts/navigation/nav-sidebar-program-coordinator.blade.php
@@ -109,7 +109,7 @@
                         class="absolute w-48 py-1 mt-2 origin-bottom-left bg-white rounded-md shadow-lg left-10 bottom-14 focus:outline-none"
                         role="menu" aria-orientation="vertical" aria-label="user menu" >
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
+                
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
