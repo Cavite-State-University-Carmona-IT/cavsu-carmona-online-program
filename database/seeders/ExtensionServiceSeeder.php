285efc1fba38c19573daf17457b100aa177a5382
gezrylclariz <gezrylclarizg@gmail.com>
2021-10-19 16:24:29 +0800

    image file extension name

--- database/seeders/ExtensionServiceSeeder.php
+++ database/seeders/ExtensionServiceSeeder.php
@@ -18,27 +18,27 @@ class ExtensionServiceSeeder extends Seeder
             [
                 'name' => 'BARANGAY ENTREPRENYUR',
                 'department_id' => 1,
-                'image' => 'BARANGAY_ENTREPRENYUR'
+                'image' => 'BARANGAY_ENTREPRENYUR.png'
             ],
             [
                 'name' => 'BASURA KO, AYOS KO',
                 'department_id' => 2,
-                'image' => 'BASURA_KO_AYOS_KO'
+                'image' => 'BASURA_KO_AYOS_KO.png'
             ],
             [
                 'name' => 'KAKAYAHANG TEKNIKAL TUNGO SA MAGANDANG KINABUKASAN',
                 'department_id' => 3,
-                'image' => 'KAKAYAHANG_TEKNIKAL_TUNGO_SA_MAGANDANG_KINABUKASAN'
+                'image' => 'KAKAYAHANG_TEKNIKAL_TUNGO_SA_MAGANDANG_KINABUKASAN.png'
             ],
             [
                 'name' => 'PROJECT KOMPYUTER',
                 'department_id' => 4,
-                'image' => 'PROJECT_KOMPYUTER'
+                'image' => 'PROJECT_KOMPYUTER.png'
             ],
             [
                 'name' => 'PROJECT PISARA',
                 'department_id' => 5,
-                'image' => 'PROJECT_PISARA'
+                'image' => 'PROJECT_PISARA.png'
             ],
         ];
         ExtensionService::insert($data);
