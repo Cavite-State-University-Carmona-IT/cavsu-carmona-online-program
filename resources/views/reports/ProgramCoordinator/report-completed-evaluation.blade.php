9bd7c87623f40b1463c2cde128b3f5a7b644d883
VanAndrewMicabani <91940048+VanAndrewMicabani@users.noreply.github.com>
2021-10-21 13:43:19 +0800

    wip-report-of-completed-evaluation

--- resources/views/reports/ProgramCoordinator/report-completed-evaluation.blade.php
+++ resources/views/reports/ProgramCoordinator/report-completed-evaluation.blade.php
@@ -1,56 +1,63 @@
 <div style="width:80%;">
-	<table width="100%" border="0" cellpadding="2" cellspacing="1" style="width:836pt; line-height:16pt;">
-		<tr><td style="border:none; font-size:12pt;" align="middle" colspan="5"><strong>CAVITE STATE UNIVERSITY</strong></td></tr>
-		<tr><td style="border:none; font-size:12pt;" align="middle" colspan="5">Completed Evaluation Report</td></tr>
+	<table width="100%" border="0" cellpadding="2" cellspacing="1" style="width:836pt; line-height:16pt; text-decoration:underline;">
+		<tr><td style="border:none; font-size:12pt; font-weight: bold;" align="middle" colspan="6">Cavite State University</td></tr>
+		<tr><td style="border:none; font-size:12pt; font-weight: bold;" align="middle" colspan="6">Completed Evaluation Report</td></tr>
 		<tr>
             <td style="border:none; font-size:12pt;" align="left" colspan="5">Rundate : {{ carbon\Carbon::now()->format('M d Y - h:i a') }}</td>
         </tr>
 		<tr>
-		    <td style="border:none; font-size:12pt;" align="left" colspan="5">Run by : {{ Auth::user()->getFullNameLNfirst() }}</td>
+		    <td style="border:none; font-size:12pt; text-transform:capitalize;" align="left" colspan="5">Run by : {{ Auth::user()->getFullNameLNfirst() }}</td>
         </tr>
 	</table>
 
 	<table width="100%" border="1" cellpadding="2" cellspacing="1" style="font-size:10pt; width:836pt; line-height:16pt;">
-
+    <?php $i=0; ?>
         @foreach($data as $key => $extension_service)
-            <tr>
-                <td style="border-bottom:none;" colspan="5"><strong>Extension Service: {{ $key }}</strong></td>
-            </tr>
-            {{-- lagay mo yung column name dto na need, like webinar title --}}
+        @if($i==1)<tr>
+                    <td style="text-align:center;" colspan="6">--------------------------------------</td>
+                </tr>@endif
             <tr style="text-align: center;">
-                <td style="border-bottom:none;" rowspan="2"><strong>Webinar Title</strong></td>
-                <td style="border-bottom:none;" rowspan="2"><strong>OR NUMBER</strong></td>
-                <td style="border-bottom:none;" colspan="2"><strong>ACCOUNT</strong></td>
-                <td style="border-bottom:none;" rowspan="2"><strong>AMOUNT</strong></td>
+                <td style="border-bottom:none;" colspan="6" ><strong>Extension Service: {{ $key }}</strong></td>
             </tr>
             <tr style="text-align: center;">
-                <td style="border-bottom:none;"><strong>CODE</strong></td>
-                <td style="border-bottom:none;"><strong>NAME</strong></td>
+                <td style="border-bottom:none;" colspan="2"><strong>WEBINAR</strong></td>
+                <td style="border-bottom:none;" colspan="3"><strong>Name</strong></td>
+                <td style="border-bottom:none;" rowspan="2"><strong>Date Completed</strong></td>
             </tr>
-            {{-- hanggang dto --}}
+            <tr style="text-align: center;">
+                <td style="border-bottom:none;" ><strong>Title</strong></td>
+                <td style="border-bottom:none;" ><strong>Speaker</strong></td>
+                <td style="border-bottom:none;" ><strong>Last Name</strong></td>
+                <td style="border-bottom:none;" ><strong>First Name</strong></td>
+                <td style="border-bottom:none;" ><strong>Middle Initial</strong></td>
+            </tr >
             @foreach($extension_service as $webinar)
-                <tr>
-                    <td style="border-bottom:none;" colspan="5"><strong>Webinar Title: {{ $webinar[0]->title }}</strong></td>
-                    {{-- dagdagan mo pa to based dun sa header --}}
+                <?php
+                    $count = $webinar->count() + 1;
+                ?>
+                <tr style="text-align: center;">
+                    <td style="border-bottom:none;"  rowspan="{{ $count }}"><strong>{{$webinar[0]->title}}</strong></td>
+                    <td style="border-bottom:none;"  rowspan="{{ $count }}"><strong>{{$webinar[0]->speaker}}</strong></td>
                 </tr>
-                {{-- dito yung users --}}
-                @foreach($webinar as $user)
-                <tr>
-                    <td>{{ $user->last_name . ', ' . $user->first_name . ' ' . $user->middle_name[0] }}</td>
-                </tr>
-                @endforeach
+                    @foreach($webinar as $user)
+                    <tr style= "text-transform:capitalize;">
+                        <td style="border-bottom:none; text-transform:capitalize;" >{{$user->last_name}}</td>
+                        <td style="border-bottom:none; text-transform:capitalize;" >{{$user->first_name}}</td>
+                        <td style="border-bottom:none; text-transform:capitalize; text-align:center;">{{$user->middle_name[0]}}</td>
+                        <td style="border-bottom:none; text-transform:capitalize;text-align:center;">{{carbon\carbon::parse($user->date_completed)->format('d M, Y');}}</td>
+                    </tr> 
+                    @endforeach
             @endforeach
+            <?php $i=1; ?>
         @endforeach
-
-        <tr><td align="right" colspan="5"></td></tr>
 	</table>
 </div>
 <?php
-    // header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    // header("Content-Disposition: attachment; filename=".$filename.".xls");
-    // header("Pragma: no-cache");
-    // header("Expires: 0");
-?>
 
-{{-- paaayos nlng layout
-    tas uncomment mo na tong header kapag gusto mo na madl yung excel file --}}
+    //---
+    header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header("Content-Disposition: attachment; filename=filename=".carbon\carbon::now()->format('dmY_') ."report_completed_evaluation.xls");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    //---
+?>
