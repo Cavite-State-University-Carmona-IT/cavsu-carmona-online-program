85071a62dd5370020128af4a3dcdabbf5c8ce9fd
MayBernadetteBautista <91201778+MayBernadetteBautista@users.noreply.github.com>
2021-10-25 21:30:42 +1400

    sidebar link to advertisement

--- resources/views/layouts/navigation/nav-sidebar-program-coordinator.blade.php
+++ resources/views/layouts/navigation/nav-sidebar-program-coordinator.blade.php
@@ -149,7 +149,7 @@
                         <img class="h-5 w-auto"  src="{{ asset('storage/image/icons/webinar.gif') }}" />
                             <span>Webinar</span>
                         </a>
-                        <a href="" class="flex items-center w-full space-x-2  p-2  border-l-2 border-transparent ">
+                        <a href="{{ route('program-coordinator.advertisement') }}" class="flex items-center w-full space-x-2  p-2  border-l-2 border-transparent ">
                         <img class="h-5 w-auto"  src="{{ asset('storage/image/icons/advertisement.gif') }}" />
                             <span>Advertisement</span>
                         </a>
