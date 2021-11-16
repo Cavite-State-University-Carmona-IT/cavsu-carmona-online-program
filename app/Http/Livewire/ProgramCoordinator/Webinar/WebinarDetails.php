23721daf3e95bc3d76db9aefd21c1146546f3e30
RolandGacula <rolandelly.gacula@cvsu.edu.ph>
2021-11-16 15:50:23 +0800

    see description
    webinar update
    *without image upload
    
    webinar delete
    *without db data deletion
    
    nxt update after webinar details task complete

--- app/Http/Livewire/ProgramCoordinator/Webinar/WebinarDetails.php
+++ app/Http/Livewire/ProgramCoordinator/Webinar/WebinarDetails.php
@@ -1,9 +1,12 @@
 <?php
-
+//Roland Elly B. Gacula 1501-00294
+//Crizaldy Derain 
+// webinar edit and delete
 namespace App\Http\Livewire\ProgramCoordinator\Webinar;
 
 use Livewire\Component;
 use Illuminate\Http\Request;
+
 
 use App\Models\Webinar;
 
@@ -11,7 +14,18 @@ class WebinarDetails extends Component
 {
     public $webinar;
     public $title;
+    public $speaker;
+    public $image;
+    public $about;
+    public $video_link;
+    public $date;
+    public $evaluation_link;
+    public $ecertificate_link;
+    public $delete;
     public $test;
+    
+    
+    
 
     public function mount(Request $request)
     {
@@ -20,6 +34,13 @@ class WebinarDetails extends Component
         ->first();
 
         $this->title = $this->webinar->title;
+        $this->speaker = $this->webinar->speaker;
+        $this->image = $this->webinar->image;
+        $this->about = $this->webinar->about;
+        $this->video_link = $this->webinar->video_link;
+        $this->date = $this->webinar->date;
+        $this->evaluation_link = $this->webinar->evaluation_link;
+        $this->ecertificate_link = $this->webinar->ecertificate_link;
     }
 
     public function render()
@@ -32,8 +53,28 @@ class WebinarDetails extends Component
     {
         $webinar = $this->webinar;
         $webinar->title = $this->title;
+        $webinar->speaker = $this->speaker;
+        $webinar->image = $this->image;
+        $webinar->about = $this->about;
+        $webinar->video_link = $this->video_link;
+        $webinar->date = $this->date;
+        $webinar->evaluation_link = $this->evaluation_link;
+        $webinar->ecertificate_link = $this->ecertificate_link;
         $webinar->save();
     }
-
+public function delete()
+{
+    $this->title = "";
+    $this->speaker = "";
+    $this->image = "";
+    $this->about = "";
+    $this->video_link = "";
+    $this->date = "";
+    $this->evaluation_link = "";
+    $this->ecertificate_link = "";
+    $this->webinar->delete();
+   
+    
+}
 
 }
--- resources/views/livewire/program-coordinator/webinar/webinar-details.blade.php
+++ resources/views/livewire/program-coordinator/webinar/webinar-details.blade.php
@@ -4,11 +4,127 @@
         <div class="col-span-6 col-start-2">
             <div class="grid w-full grid-cols-3">
                 <div class="col-span-1 bg-white shadow-md rounded-xl">
-                    <div class="max-w-full">
-                        <input wire:model="title" type="text">
-                        <button wire:click="update()" class="p-2 pl-5 pr-5 text-lg text-gray-100 bg-blue-500 border-blue-300 rounded-lg focus:border-4">Update</button>
-                    </div>
+
+                <div class="shadow sm:rounded-md sm:overflow-hidden">
+          <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company_website" class="block text-sm font-medium text-gray-700">
+                title
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <input wire:model="title" type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
                 </div>
+              </div>
+            </div>
+
+            <div>
+              <label class="block text-sm font-medium text-gray-700">
+                image
+              </label>
+              <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                <div class="space-y-1 text-center">
+                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  <div class="flex text-sm text-gray-600">
+                    <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                      <span>Upload a file</span>
+                      <input wire:model="image" id="file-upload" name="file-upload" type="file" class="sr-only">
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs text-gray-500">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label for="about" class="block text-sm font-medium text-gray-700">
+                About
+              </label>
+              <div class="mt-1">
+                <textarea wire:model="about" id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
+              </div>
+              <p class="mt-2 text-sm text-gray-500">
+                Brief description for your profile. URLs are hyperlinked.
+              </p>
+            </div>
+
+            <div class="col-span-3 sm:col-span-2">
+                <label for="company_website" class="block text-sm font-medium text-gray-700">
+                speaker
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <input wire:model="speaker" type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                </div>
+              </div>
+
+              <div class="relative">
+                 <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                 </div>
+                  <input wire:model="date" datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Select date">
+            </div>
+
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company-website" class="block text-sm font-medium text-gray-700">
+                video link 
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                    https://www.youtube.com/embed/
+                  </span>
+                  <input wire:model="video_link" type="text" name="company-website" id="company-website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                </div>
+              </div>
+
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company_website" class="block text-sm font-medium text-gray-700">
+                evaluatino_link 
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                    http://
+                  </span>
+                  <input wire:model="evaluation_link" type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                </div>
+              </div>
+
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company_website" class="block text-sm font-medium text-gray-700">
+                certificate_link 
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                    http://
+                  </span>
+                  <input wire:model="ecertificate_link" type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                </div>
+              </div>
+          </div>
+     
+            <div class="flex mt-5">
+          <div class="m-3">
+            <button
+            wire:click="update()" class="w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+              <span class="mx-auto">save</span>
+            </button>
+          </div>
+          <div class="m-3">
+            <button
+            wire:click="delete()" class="w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+              <span class="mx-auto">delete</span>
+
+            </button>
+          </div>
+        </div>
+
+          
+        </div>
+       </div>
                 <div class="col-span-2 bg-white shadow-md rounded-xl">
                     <div class="videoWrapper">
                         <iframe class="rounded-xl" width="560" height="349" src="https://www.youtube.com/embed/{{ $webinar->video_link }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
