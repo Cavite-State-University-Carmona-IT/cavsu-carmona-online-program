e1e54ec27203fe15fea6fce03735fbbc1b284600
gezrylclariz <gezrylclarizg@gmail.com>
2021-12-14 17:01:05 +0800

    from database image name and details extension service

--- resources/views/livewire/participant/extension-services-section.blade.php
+++ resources/views/livewire/participant/extension-services-section.blade.php
@@ -1,42 +1,42 @@
 <div class="max-w-full lg:py-5">
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-    
+
     <section class="w-full px-10 py-14 md:px-28 bg-gray-200">
-        
+
             <section class="grid grid-cols-1 gap-7 sm:grid-cols-2 md:grid-cols-3 md:gap-5 lg:grid-cols-5">
                 <article class="w-full bg-transparent">
                     <a href="{{ url('extension-service/barangay-entreprenyur') }}" class="">
-                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/bw-BARANGAY_ENTREPRENYUR.png') }}"/>
+                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/'.$this->ext1->image) }}"/>
                         <p class="font-bold">Barangay Entreprenyur</p>
-                        <p class="text-gray-500">Total of 8900 webinars and 200,990 students enrolled.</p>
+                        <p class="text-gray-500">{{ $this->ext1->details }}</p>
                     </a>
                 </article>
                 <article class="w-full bg-transparent">
                     <a href="{{ url('extension-service/barangay-entreprenyur') }}" class="">
-                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/bw-BASURA_KO_AYOS_KO.png') }}"/>
+                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/'.$this->ext2->image) }}"/>
                         <p class="font-bold">Basura Ko Ayos Ko</p>
-                        <p class="text-gray-500">Total of 8900 webinars and 200,990 students enrolled.</p>
+                        <p class="text-gray-500">{{ $this->ext2->details }}</p>
                     </a>
                 </article>
                 <article class="w-full bg-transparent">
                     <a href="{{ url('extension-service/barangay-entreprenyur') }}" class="">
-                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/bw-KAKAYAHANG_TEKNIKAL_TUNGO_SA_MAGANDANG_KINABUKASAN.png') }}"/>
+                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/'.$this->ext3->image) }}"/>
                         <p class="font-bold">Kakayahang teknikal tungo sa magandang kinabukasan</p>
-                        <p class="text-gray-500">Total of 8900 webinars and 200,990 students enrolled.</p>
+                        <p class="text-gray-500">{{ $this->ext3->details }}</p>
                     </a>
                 </article>
                 <article class="w-full bg-transparent">
                     <a href="{{ url('extension-service/barangay-entreprenyur') }}" class="">
-                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/bw-PROJECT_KOMPYUTER.png') }}"/>
+                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/'.$this->ext4->image) }}"/>
                         <p class="font-bold">Project Kompyuter</p>
-                        <p class="text-gray-500">Total of 8900 webinars and 200,990 students enrolled.</p>
+                        <p class="text-gray-500">{{ $this->ext4->details }}</p>
                     </a>
                 </article>
                 <article class="w-full bg-transparent">
                     <a href="{{ url('extension-service/barangay-entreprenyur') }}" class="">
-                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/bw-PROJECT_PISARA.png') }}"/>
+                        <img class="h-24 mb-6" src="{{ asset('storage/image/extension-services/'.$this->ext5->image) }}"/>
                         <p class="font-bold">Project Pisara</p>
-                        <p class="text-gray-500">Total of 8900 webinars and 200,990 students enrolled.</p>
+                        <p class="text-gray-500">{{ $this->ext5->details }}</p>
                     </a>
                 </article>
             </section>
