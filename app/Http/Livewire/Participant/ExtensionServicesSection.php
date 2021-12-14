d4eb55b1caed838ae1508688b1319363d64802dc
gezrylclariz <gezrylclarizg@gmail.com>
2021-12-14 17:01:29 +0800

    extension service livewire controller for image and details

--- app/Http/Livewire/Participant/ExtensionServicesSection.php
+++ app/Http/Livewire/Participant/ExtensionServicesSection.php
@@ -3,11 +3,28 @@
 namespace App\Http\Livewire\Participant;
 
 use Livewire\Component;
+use App\Models\ExtensionService;
 
 class ExtensionServicesSection extends Component
 {
+    public $ext1, $ext2, $ext3, $ext4, $ext5;
+
+    public function mount()
+    {
+        $this->ext1 = $this->extValue(1);
+        $this->ext2 = $this->extValue(2);
+        $this->ext3 = $this->extValue(3);
+        $this->ext4 = $this->extValue(4);
+        $this->ext5 = $this->extValue(5);
+    }
+
     public function render()
     {
         return view('livewire.participant.extension-services-section');
+    }
+
+    public function extValue($id)
+    {
+        return ExtensionService::find($id);
     }
 }
