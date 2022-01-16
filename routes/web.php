5c8078ad65ff416733305f96ea8ef1aa1dd0a360
gezrylclariz <gezrylclarizg@gmail.com>
2022-01-16 23:29:22 +0800

    route to extension service with or without field of interest (see description)
    example
    sample.com/extension-service/project-kompyuter/
    or
    sample.com/extension-service/project-kompyuter/it-certification

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Livewire'], function() {
 
     Route::get('/', Participant\Home::class);
-    Route::get('extension-service/{name}/', Participant\ExtensionService\ExtensionServiceIndex::class);
-    Route::get('extension-service/{name}/{fieldofinterest}', Participant\ExtensionService\ExtensionServiceIndex::class);
+    Route::get('extension-service/{extension_service_link_name}/', Participant\ExtensionService\ExtensionServiceIndex::class);
+    Route::get('extension-service/{extension_service_link_name}/{field_of_interest_link_name}', Participant\ExtensionService\ExtensionServiceIndex::class);
     Route::get('webinars/search/', Participant\Search\SearchPage::class)->name('webinarsearch');
     Route::get('webinar/{title}', Participant\Webinar\WebinarIndex::class)->name('webinar');
 
