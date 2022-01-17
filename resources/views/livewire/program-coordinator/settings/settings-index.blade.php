d5e14e8f96d9417491b4faf45f650bc5cd940e19
gezrylclariz <gezrylclarizg@gmail.com>
2022-01-18 00:15:31 +0800

    Update settings-index.blade.php

--- resources/views/livewire/program-coordinator/settings/settings-index.blade.php
+++ resources/views/livewire/program-coordinator/settings/settings-index.blade.php
@@ -3,7 +3,7 @@
     <div class="max-w-full pt-16 mt-2 bg-white">
         <div class="grid grid-cols-3">
             {{-- left panel --}}
-            <div class="md:col-span-1 col-start-1 col-span-full p-4 bg-custom-gray-background">
+            <div class="md:col-span-1 col-start-1 col-span-full p-4 ">
                 @livewire('program-coordinator.settings.extension-service-section')
             </div>
             {{-- right panel --}}
