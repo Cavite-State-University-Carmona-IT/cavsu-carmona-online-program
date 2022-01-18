ff1cd6f506a9d3bc1840c4ed7909df70aebb0ce3
gezrylclariz <gezrylclarizg@gmail.com>
2022-01-18 13:51:09 +0800

    config for user variables

---
+++ config/custom-user.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User values
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default authentication "guard" and password
+    | reset options for your application. You may change these defaults
+    | as required, but they're a perfect start for most applications.
+    |
+    */
+
+    'gender' => [
+        '0' => 'Male',
+        '1' => 'Female',
+        '2' => 'Rather not say',
+    ],
+
+    'marital_status' => [
+        '1' => 'Married',
+        '2' => 'Widowed',
+        '3' => 'Separated',
+        '4' => 'Divorced',
+        '5' => 'Single',
+    ],
+
+    'employment_status' => [
+        '1' => 'Freelance',
+        '2' => 'Full-time',
+        '3' => 'Part-time',
+        '4' => 'Unemployed',
+        '5' => 'Self-employed',
+        '6' => 'Retired',
+        '7' => 'N/A',
+    ],
+
+    'highest_educational_attainment' => [
+        '1' => 'Some Elementary Graduate',
+        '2' => 'Elementary Graduate',
+        '3' => 'Some High School',
+        '4' => 'High School Graduate',
+        '5' => 'Some College/Vocational',
+        '6' => 'College Graduate',
+        '7' => "Some/Completed Master's Degree",
+        '8' => 'Masters Graduate',
+        '9' => 'Vocational/TVET',
+    ],
+
+];
